fb9a61916462c563de939a2d8eab6708e6550c3f
panpan <R-GCombat@hotmail.com>
2017-11-04 18:19:21 +0200

    Add files via upload

---
+++ paint/paint/Form1.Designer.cs
@@ -0,0 +1,254 @@
+﻿namespace paint
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.comboBox3 = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.Font = new System.Drawing.Font("Minerva", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Black",
+            "Red",
+            "Blue",
+            "Green",
+            "Yellow",
+            "Cyan",
+            "Eraser"});
+            this.comboBox1.Location = new System.Drawing.Point(744, 98);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 28);
+            this.comboBox1.TabIndex = 0;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Monotype Corsiva", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(741, 71);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(51, 18);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Colour";
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox2.Font = new System.Drawing.Font("Minerva", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Items.AddRange(new object[] {
+            "Line",
+            "Free draw",
+            "Rectangular",
+            "Ellipse",
+            "Circle"});
+            this.comboBox2.Location = new System.Drawing.Point(744, 164);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(289, 28);
+            this.comboBox2.TabIndex = 2;
+            this.comboBox2.SelectedIndexChanged += new System.EventHandler(this.comboBox2_SelectedIndexChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Monotype Corsiva", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(741, 136);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(46, 18);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Shape";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // comboBox3
+            // 
+            this.comboBox3.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox3.Font = new System.Drawing.Font("Minerva", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.comboBox3.FormattingEnabled = true;
+            this.comboBox3.Items.AddRange(new object[] {
+            "1",
+            "2",
+            "3",
+            "4",
+            "5",
+            "6",
+            "7",
+            "8",
+            "9",
+            "10"});
+            this.comboBox3.Location = new System.Drawing.Point(912, 98);
+            this.comboBox3.Name = "comboBox3";
+            this.comboBox3.Size = new System.Drawing.Size(121, 28);
+            this.comboBox3.TabIndex = 4;
+            this.comboBox3.SelectedIndexChanged += new System.EventHandler(this.comboBox3_SelectedIndexChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Monotype Corsiva", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(909, 71);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(56, 18);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Width ";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Minion Pro SmBd", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(816, 66);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(56, 26);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Black";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Minion Pro SmBd", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(994, 63);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(21, 26);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "4";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Minion Pro SmBd", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(794, 132);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(171, 29);
+            this.label6.TabIndex = 8;
+            this.label6.Text = "No Shape Selected";
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 600;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.Highlight;
+            this.button1.Font = new System.Drawing.Font("Moonbeam", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(799, 286);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(181, 58);
+            this.button1.TabIndex = 10;
+            this.button1.Text = "Snowflake";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.button2.Font = new System.Drawing.Font("Moonbeam", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(749, 222);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(123, 58);
+            this.button2.TabIndex = 11;
+            this.button2.Text = "Snowman";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.SystemColors.HotTrack;
+            this.button3.Font = new System.Drawing.Font("Moonbeam", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.Location = new System.Drawing.Point(891, 222);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(124, 58);
+            this.button3.TabIndex = 12;
+            this.button3.Text = "tree";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.ClientSize = new System.Drawing.Size(1106, 561);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.comboBox3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Click += new System.EventHandler(this.Form1_Click);
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseDown);
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseMove);
+            this.MouseUp += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseUp);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox comboBox3;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ paint/paint/Form1.cs
@@ -0,0 +1,454 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace paint
+{
+    public partial class Form1 : Form
+    {
+        Graphics g;
+        Pen p;
+        float x1, y1, x2, y2;
+        bool flag = false;
+        bool trl = false;
+        int i = 0;
+        int s = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            g = this.CreateGraphics();
+            p = new Pen(Color.Black);
+           
+            p.Width = 5;
+
+            
+        }
+
+        private void Form1_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (comboBox2.Text == "Free draw")
+            {
+                x1 = e.X;
+                y1 = e.Y;
+                flag = true;
+            }
+            else
+            {
+                x1 = e.X;
+                y1 = e.Y;
+                
+            }
+           
+        }
+
+        private void Form1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (comboBox2.Text == "Free draw")
+            {
+                if (flag)
+                {
+                    g.DrawLine(p, x1, y1, e.X, e.Y);
+                    x1 = e.X;
+                    y1 = e.Y;
+                }
+
+            }
+        }
+
+        private void comboBox3_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            float z = float.Parse(comboBox3.Text);
+            p.Width = z;
+            label5.Text = comboBox3.Text;
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            label6.Text = comboBox2.Text;
+        }
+
+        private void label7_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            s = 1;
+         
+            timer1.Enabled = true;
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (s == 1)
+            {
+                if (i == 0)
+                {
+                    g.DrawLine(p, 150, 150, 100, 150);
+                }
+                if (i == 1)
+                {
+                    g.DrawLine(p, 150, 150, 150, 100);
+                }
+                else if (i == 2)
+                {
+                    g.DrawLine(p, 150, 150, 200, 150);
+                }
+                else if (i == 3)
+                {
+                    g.DrawLine(p, 150, 150, 150, 200);
+                }
+                else if (i == 4)
+                {
+                    g.DrawLine(p, 110, 110, 150, 150);
+                }
+                else if (i == 5)
+                {
+                    g.DrawLine(p, 190, 190, 150, 150);
+                }
+                else if (i == 6)
+                {
+                    g.DrawLine(p, 150, 150, 110, 190);
+                }
+                else if (i == 7)
+                {
+                    g.DrawLine(p, 150, 150, 190, 110);
+
+                }
+                else if (i == 8)
+                {
+                    g.DrawLine(p, 125, 125, 106, 125);
+                }
+                else if (i == 9)
+                {
+                    g.DrawLine(p, 125, 125, 125, 106);
+                }
+                else if (i == 10)
+                {
+                    g.DrawLine(p, 115, 150, 108, 135);
+
+                }
+                else if (i == 11)
+                {
+                    g.DrawLine(p, 115, 150, 108, 165);
+
+                }
+                else if (i == 12)
+                {
+                    g.DrawLine(p, 125, 125, 106, 125);
+
+                }
+                else if (i == 13)
+                {
+                    g.DrawLine(p, 125, 175, 106, 175);
+
+                }
+                else if (i == 14)
+                {
+                    g.DrawLine(p, 125, 175, 125, 194);
+
+                }
+                else if (i == 15)
+                {
+                    g.DrawLine(p, 185, 150, 192, 165);
+
+                }
+                else if (i == 16)
+                {
+                    g.DrawLine(p, 185, 150, 192, 135);
+
+                }
+                else if (i == 17)
+                {
+                    g.DrawLine(p, 175, 125, 194, 125);
+
+                }
+                else if (i == 18)
+                {
+                    g.DrawLine(p, 175, 125, 175, 106);
+
+                }
+                else if (i == 19)
+                {
+                    g.DrawLine(p, 175, 175, 194, 175);
+
+                }
+                else if (i == 20)
+                {
+                    g.DrawLine(p, 175, 175, 175, 194);
+
+                }
+                else if (i == 21)
+                {
+                    g.DrawLine(p, 150, 119, 137, 103);
+                }
+                else if (i == 22)
+                {
+                    g.DrawLine(p, 150, 119, 163, 103);
+
+                }
+                else if (i == 23)
+                {
+                    g.DrawLine(p, 150, 181, 137, 197);
+
+                }
+                else if (i == 24)
+                {
+                    g.DrawLine(p, 150, 181, 163, 197);
+
+                }
+                else if (i == 25)
+                {
+                    timer1.Enabled = false;
+                    i = -1;
+                }
+            }
+            if (s == 2)
+
+            {
+                if (i == 0)
+                {
+                    g.DrawEllipse(p, 150, 350, 200, 200);
+                }
+                else if (i == 1)
+                {
+                    g.DrawEllipse(p, 175, 200, 150, 150);
+                }
+                else if (i == 2)
+                {
+                    g.DrawEllipse(p, 200, 100, 100, 100);
+                }
+                else if (i == 3)
+                {
+                    g.DrawLine(p, 175, 100, 325, 100);
+                }
+                else if ( i == 4)
+                {
+                    g.DrawLine(p, 220, 100, 220, 50);
+                }
+                else if (i == 5)
+                {
+                    g.DrawLine(p, 220, 50, 290, 50);
+                }
+                else if (i == 6)
+                {
+                    g.DrawLine(p, 290, 50, 290, 100);
+                }
+                else if (i == 7)
+                {
+                    g.DrawEllipse(p, 237, 133, 12, 12);
+                        
+                }
+                else if (i == 8)
+                {
+                    g.DrawEllipse(p, 260, 133, 12, 12);
+                }
+                else if (i == 9)
+                {
+                    g.DrawLine(p, 250, 145, 270, 155);
+                }
+                else if (i == 10)
+                {
+                    g.DrawLine(p, 270, 155, 250, 165);
+
+                }
+                else if (i == 11)
+                {
+                    g.DrawLine(p, 225, 175, 275, 175);
+                }
+                else if (i == 12)
+                {
+                    g.DrawLine(p, 184, 241, 126, 311);
+                }
+                else if (i == 13)
+                {
+                    g.DrawLine(p, 148, 283, 119, 290);
+                }
+                else if (i == 14)
+                {
+                    g.DrawLine(p, 138, 298, 150, 308);
+                }
+                else if (i == 15)
+                {
+                    g.DrawLine(p, 321, 252, 367, 297);
+                }
+                else if (i == 16)
+                {
+                    g.DrawLine(p, 346, 277, 371, 287);
+                }
+                else if (i == 17)
+                {
+                    timer1.Enabled = false;
+                    i = -1;
+                }
+            }
+            if (s == 3)
+            {
+                if (i == 0) {
+                    g.DrawLine(p, 239, 206, 162, 284);
+                }
+                else if (i == 1)
+                {
+                    g.DrawLine(p, 162, 284, 226, 286);
+                }
+                else if (i == 2)
+                {
+                    g.DrawLine(p, 226, 286, 117, 378);
+                }
+                else if (i == 3)
+                {
+                    g.DrawLine(p, 117, 378, 205, 376);
+                }
+                else if (i == 4)
+                {
+                    g.DrawLine(p, 205, 376, 62, 486);
+                }
+                else if (i == 5)
+                {
+                    g.DrawLine(p, 62, 486, 440, 486);
+                }
+                else if (i == 6)
+                {
+                    g.DrawLine(p, 440, 486, 260, 371);
+                }
+                else if (i == 7)
+                {
+                    g.DrawLine(p, 260, 371, 370, 360);
+                }
+                else if (i == 8)
+                {
+                    g.DrawLine(p, 370, 360, 273, 284);
+
+                }
+                else if (i == 9)
+                {
+                    g.DrawLine(p, 273, 284, 318, 277);
+                }
+                else if (i == 10)
+                {
+                    g.DrawLine(p, 318, 277, 239, 206);
+                }
+                else if (i == 11)
+                {
+                    g.DrawLine(p, 205, 482, 205, 552);
+
+                }
+                else if (i == 12)
+                {
+                    g.DrawLine(p, 205, 552, 277, 552);
+                }
+                else if (i == 13)
+                {
+                    g.DrawLine(p, 277, 552, 277, 482);
+                }
+                else if (i == 14)
+                {
+                    timer1.Enabled = false;
+                    i = -1;
+                }
+            }
+            i = i + 1;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            s = 2;
+            timer1.Enabled = true;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            s = 3;
+            timer1.Enabled = true;
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (comboBox1.Text == "Black")
+            {
+                p.Color = Color.Black;
+            }
+           else if (comboBox1.Text == "Red")
+            {
+                p .Color = Color.Red;
+            }
+            else if (comboBox1.Text == "Blue")
+            {
+                p.Color = Color.Blue;
+            }
+            else if (comboBox1.Text == "Green")
+            {
+                p.Color = Color.Green;
+            }
+            else if (comboBox1.Text == "Yellow")
+            {
+                p.Color = Color.Yellow;
+            }
+            else if (comboBox1.Text == "Cyan")
+            {
+                p.Color = Color.Cyan;
+            }
+            else if (comboBox1.Text == "Eraser")
+            {
+                p.Color = Color.White;
+            }
+            label4.Text = comboBox1.Text;
+           
+        }
+
+        private void Form1_Click(object sender, EventArgs e)
+        {
+            g = this.CreateGraphics();
+            trl = !trl;
+
+        }
+
+        private void Form1_MouseUp(object sender, MouseEventArgs e)
+        {
+            x2 = e.X;
+            y2 = e.Y;
+            float w = x2 - x1;
+            float h = y2 - y1;
+            if (comboBox2.Text == "Line")
+            {
+                g.DrawLine(p, x1, y1, x2, y2);
+            }
+            else if (comboBox2.Text == "Free draw")
+            {
+                flag = false;
+            }
+            else if (comboBox2.Text == "Rectangular")
+            {
+                g.DrawRectangle(p, x1, y1, w, h);
+            }
+            else if (comboBox2.Text == "Ellipse")
+            {
+                g.DrawEllipse(p, x1, y1, w, h);
+            }
+            else if(comboBox2.Text == "Circle")
+            {
+                g.DrawEllipse(p, x1, y1, w, h);
+            
+            }
+        }
+    }
+}
